621795a959c9b8f62deceaec20fa74e3d8ce1894
barbosajuao <121351231+barbosajuao@users.noreply.github.com>
2024-08-18 20:35:16 -0300

    asdasdasd

---
+++ land-page/js/slider.js
@@ -0,0 +1,11 @@
+document.addEventListener(`DOMContentLoaded`, function() {
+    
+    var valorindex = 0
+    var slider = window.documentElement.querySelectorAll(`.slider`)
+    var ponto = window.documentElement.querySelectorAll(`.ponto`)
+    var totalslider = slider.length
+
+    
+})
+
+
